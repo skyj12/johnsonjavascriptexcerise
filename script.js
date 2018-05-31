98cc424a60e98345b10b03a2bafc2773fa8d0a7f
Vera Johnson <verajohnson@Veras-MacBook-Pro.local>
2018-05-31 12:00:03 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// var num = prompt("Enter a Number");
+// var total = 0;
+// for (var x = 0; x<=num; x++) {
+// total += x;
+// }
+// console.log(total)
+
+// var play = prompt("do you want to play?")
+//   if (play == "yes" ) {
+//   do { var word = prompt ("Enter a Word");
+//         document.write(dog);
+//         var val = prompt ("do you want to play again?");
+//   } while (val == "yes")
+//   do { var word = prompt ("Enter a Word");
+//         document.write(cat);
+//         var val = prompt ("do you want to play again?");
+//   } while (val == "no")
+//         document.write(dogcat);
+//   }
+
+// var answer = prompt("Would you like to print your name?");
+//  var name = "Hello. My name is Adam";
+//  if (answer == "yes" ) {
+//    do { 
+//     console.log(name + "!")
+//  var val = prompt ("Would you like to print this again?");
+//  var name = "Hello. My name is Adam";
+//    } while (val == "yes") {
+//    do { 
+//     console.log(name + "!")}
+//         var val = prompt ("Would you like to print this again?");
+//          var name = "Hello. My name is Adam";
+//    } while (val == "yes") {
+//    do { 
+//     console.log(name + "!")}
+//      var val = prompt ("Would you like to print this again?");
+//      var name = "Hello. My name is Adam";
+//  if (answer == "no" ) {
+//    do { 
+//     console.log(name + "!")
+//   }
+
+
+
+
+// }
+// var input = prompt("Please enter a time of day (mornining, noon, or evening).");
+//     if (input == "morning") {
+//     console.log("Since it is morning, you should be eating breakfast. We suggest eggs and toast.");
+//     }else if(input == "evening") {
+//     console.log("Since it is evening, you should be eating dinner. We suggest chicken and rice.")
+//     }else if(input == "noon"){  
+//       console.log("Since it is noon, you should be eating lunch. We suggest a salad.")
+//     }else{
+//       console.log("Not a valid entry, please enter either morning, noon, or evening")
+//     }
